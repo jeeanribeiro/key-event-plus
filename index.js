bbaa3ba52e9e07d456e77d5d6b719628cd5feb82
Jean Ribeiro <jeanmendesrib@gmail.com>
2020-06-15 18:45:52 -0300

    Formats code

--- index.js
+++ index.js
@@ -1,58 +1,61 @@
 module.exports = (ms, callback) => {
 
-	let keyPress = {}, keyUpDown = {}, keyPlus = {}, isUpdatable = false;
+  let keyPress = {},
+    keyUpDown = {},
+    keyPlus = {};
 
-	const keyPressListener = () => {
-		document.addEventListener('keypress', event => {
-			keyPress = {
-				key: event.key,
-				code: event.code,
-			}
-		})
-	}
-	keyPressListener();
+  const keyPressListener = () => {
+    document.addEventListener('keypress', event => {
+      keyPress = {
+        key: event.key,
+        code: event.code,
+      }
+    })
+  }
+  keyPressListener();
 
-	const keyDownListener = () => {
-		document.addEventListener('keydown', event => {
-			keyUpDown = {
-				key: event.key,
-				code: event.code,
-				state: 'down',
-			}
-		})
-	}
-	keyDownListener();
+  const keyDownListener = () => {
+    document.addEventListener('keydown', event => {
+      keyUpDown = {
+        key: event.key,
+        code: event.code,
+        state: 'down',
+      }
+    })
+  }
+  keyDownListener();
 
-	const keyUpListener = () => {
-		document.addEventListener('keyup', event => {
-			keyUpDown = {
-				key: event.key,
-				code: event.code,
-				state: 'up',
-			}
-		})
-	}
-	keyUpListener();
+  const keyUpListener = () => {
+    document.addEventListener('keyup', event => {
+      keyUpDown = {
+        key: event.key,
+        code: event.code,
+        state: 'up',
+      }
+    })
+  }
+  keyUpListener();
 
-	const updateKeyPlus = () => {
-		if (keyPress.code === keyUpDown.code) {
-			keyPlus.key = keyPress.key;
-			keyPlus.state = keyUpDown.state;
-		} else {
-			keyPlus.key = keyUpDown.key;
-			keyPlus.state = keyUpDown.state;
-		}
-		return keyPlus;
-	}
+  const updateKeyPlus = () => {
+    if (keyPress.code === keyUpDown.code) {
+      keyPlus.key = keyPress.key;
+      keyPlus.state = keyUpDown.state;
+    } else {
+      keyPlus.key = keyUpDown.key;
+      keyPlus.state = keyUpDown.state;
+    }
+    return keyPlus;
+  }
 
-	const keyListenerLoop = () => {
-		const hasKeyChanged = keyPlus.key !== keyPress.key && keyPlus.key !== keyUpDown.key;
-		const hasStateChanged = keyPlus.state !== keyUpDown.state;
+  const keyListenerLoop = () => {
+    const hasKeyChanged = keyPlus.key !== keyPress.key && keyPlus.key !== keyUpDown.key;
+    const hasStateChanged = keyPlus.state !== keyUpDown.state;
 
-		if (hasKeyChanged || hasStateChanged) {
-			callback(updateKeyPlus());
-		}
-		setTimeout(keyListenerLoop, ms);
-	}
-	keyListenerLoop();
+    if (hasKeyChanged || hasStateChanged) {
+      callback(updateKeyPlus());
+    }
+    setTimeout(keyListenerLoop, ms);
+  }
+  keyListenerLoop();
+
 };
